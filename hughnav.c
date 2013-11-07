e2d72fcccf6be8df428be8188fcf7c7556e3d5cb
Hugh O'Brien <obrien.hugh@gmail.com>
2013-11-07 00:58:34 +0000

    clean and rename

--- hughnav.c
+++ hughnav.c
@@ -1,40 +1,36 @@
 #include <linux/module.h>
-//#include <linux/init.h> // seems unnecessary
-//#include <linux/kernel.h> // seems unnecessary
-#include "../ath9k/debug.h"
-#include "../ath9k/reg.h"
-#include "../ath9k/hw.h"
 
 static int hughnav_value = 0;
-int nav_update_interval __read_mostly = HZ/30;
-struct delayed_work nav_read_work;
+int hughnav_interval __read_mostly = HZ/30;
+struct delayed_work hughnav_work;
 
 extern int read_hughnav(void);
 
-static void nav_reading_update(struct work_struct *w)
+static void hughnav_update(struct work_struct *w)
 {
 	hughnav_value = read_hughnav(); // call update function here
 	printk(KERN_ALERT "hughnav running, nav:%d\n", hughnav_value);
-	schedule_delayed_work(&nav_read_work, nav_update_interval); // reschedule
+	schedule_delayed_work(&hughnav_work, hughnav_interval); // reschedule
 }
 
-static int start_nav_update_timer(void) // setup periodic reading
+static int start_hughnav_update_timer(void) // setup periodic reading
 {
-	INIT_DEFERRABLE_WORK(&nav_read_work, nav_reading_update); //macro to setup the work?
-	schedule_delayed_work(&nav_read_work, nav_update_interval);
+	INIT_DEFERRABLE_WORK(&hughnav_work, hughnav_update); //macro to setup the work?
+	schedule_delayed_work(&hughnav_work, hughnav_interval);
 	return 0;
 }
 
 static int hughnav_init(void)
 {
-	printk(KERN_ALERT "hughnav started, nav:%d, interval=%d\n", hughnav_value, nav_update_interval);
-	start_nav_update_timer();
+	printk(KERN_ALERT "hughnav started, nav:%d, interval=%d\n",\
+			hughnav_value, hughnav_interval);
+	start_hughnav_update_timer();
 	return 0;
 }
 
 static void hughnav_exit(void)
 {
-	cancel_delayed_work(&nav_read_work);
+	cancel_delayed_work(&hughnav_work);
 	printk(KERN_ALERT "hughnav exited, nav:%d\n", hughnav_value);
 }
 
